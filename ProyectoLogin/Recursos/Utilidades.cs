49f8d85448558398b47884338bd16d6ff367c808
GabrielLizaola <gabriel.lizaola@uabc.edu.mx>
2024-02-29 21:22:20 -0800

    cambios

--- ProyectoLogin/Recursos/Utilidades.cs
+++ ProyectoLogin/Recursos/Utilidades.cs
@@ -28,7 +28,7 @@ namespace ProyectoLogin.Recursos
             int i = 0;
             foreach (char c in clave)
             {
-                mensajeCifrado[i] = (int)c; // Use ASCII code directly
+                mensajeCifrado[i] = (int)c; 
                 i++;
             }
 
@@ -52,7 +52,7 @@ namespace ProyectoLogin.Recursos
             long n = p * q;
             long r = (p - 1) * (q - 1);
 
-            long d = ModInverse(e, r); // Calcular el inverso multiplicativo de e módulo r
+            long d = ModInverse(e, r); 
 
             string[] partes = claveCifrada.Split(' ');
 
@@ -68,7 +68,6 @@ namespace ProyectoLogin.Recursos
                     }
                     else
                     {
-                        // Si una parte no puede ser convertida a un número, puedes manejar el error según sea necesario
                     }
                 }
             }
@@ -85,7 +84,6 @@ namespace ProyectoLogin.Recursos
             return string.Concat(mensajeDescifrado);
         }
 
-        // Método para calcular el inverso multiplicativo módulo m
         public static long ModInverse(long a, long m)
         {
             for (long x = 1; x < m; x++)
